a3d273fb0d980c4c66cac68934078e77eebddaf0
Tuan Nguyen AMIT <nguyentuanamit@gmail.com>
2024-07-31 21:01:31 +0700

    21/7/2024

--- src/pages/DetailInteriorDesign.jsx
+++ src/pages/DetailInteriorDesign.jsx
@@ -2,22 +2,31 @@ import { useParams } from "react-router-dom";
 import Seo from "../components/Seo";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { Button, Flex } from "antd";
 
 const DetailInteriorDesign = () => {
   const { slug } = useParams();
-  const token =
-    "95da257ea041b4d78432a71330eca5ee77eaac7c4fc5e0030cd2dac010aad2b024f4f978a42cac0305b88b0fc2856671251a3c9047e92e6a9da8084908d67a77fe7be88bc0cbedd0c572515965d58916682255f24c13ec971efda545c8b2989e1ab58d74b97d6bb924aaede0142533aa094011d4e21c8002db230734645fc2f2";
+  const token = import.meta.env.VITE_TOKEN;
   const [data, setData] = useState([]);
+  const [image, setImage] = useState(null);
+  const searchData = {
+    populate: ["seo.avatar", "banner.seo.avatar", "banner.image"].toString(),
+  };
+  const searchParams = new URLSearchParams(searchData).toString();
   const getData = async () => {
     try {
-      const res = await axios.get(`http://localhost:1337/api/bai-viets`, {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      });
+      const res = await axios.get(
+        `${import.meta.env.VITE_URL_BE}/api/bai-viets/2?${searchParams}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
       const resData = await res.data;
       setData(resData);
+      setImage(resData.data.attributes.seo.avatar.data.attributes.url);
     } catch (error) {
       console.log(error);
     }
@@ -25,14 +34,29 @@ const DetailInteriorDesign = () => {
   useEffect(() => {
     getData();
   }, []);
+  console.log(data?.data?.attributes?.seo?.avatar?.data?.attributes?.url);
 
-  console.log(data);
   return (
     <>
       <Seo />
       <h1 className="flex justify-center text-[32px] text-[#4920ff] font-bold">
         {slug}
       </h1>
+      {data && (
+        <img
+          src={`${import.meta.env.VITE_URL_BE}${image}`}
+          alt={data?.data?.attributes?.seo?.title}
+        />
+      )}
+      <Flex gap="small" wrap>
+        <Button autoInsertSpace={false} size="large" type="primary">
+          Primary Button
+        </Button>
+        <Button>Default Button</Button>
+        <Button type="dashed">Dashed Button</Button>
+        <Button type="text">Text Button</Button>
+        <Button type="link">Link Button</Button>
+      </Flex>
     </>
   );
 };
