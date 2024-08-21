ab58a00695a47e701616c659d45a678c52d6abd3
jangnguyen1st <jangnguyen1st@gmail.com>
2024-08-21 21:43:11 +0700

    update: footer.jsx 210824

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { faSquarePhoneFlip } from "@fortawesome/free-solid-svg-icons";
 const Footer = () => {
   return (
     <>
@@ -5,9 +8,42 @@ const Footer = () => {
         <div className="grid grid-cols-12 gap-4">
           <div className="col-span-4">
             <h5 className="text-[28px] uppercase font-bold">
-              {" "}
               THÔNG TIN LIÊN HỆ
             </h5>
+            <h6>CÔNG TY TNHH XÂY DỰNG – TTNT LOTUS</h6>
+            <div className="flex items-center gap-4">
+              <img
+                src="https://www.lotusarchvn.com/images/icon-facebook.svg"
+                alt=""
+              />
+              <img
+                src="https://www.lotusarchvn.com/images/icon-facebook.svg"
+                alt=""
+              />
+              <img
+                src="https://www.lotusarchvn.com/images/icon-facebook.svg"
+                alt=""
+              />
+              <img
+                src="https://www.lotusarchvn.com/images/icon-facebook.svg"
+                alt=""
+              />
+              <img
+                src="https://www.lotusarchvn.com/images/icon-facebook.svg"
+                alt=""
+              />
+            </div>
+            <div className="flex items-center">
+              <FontAwesomeIcon className="pr-[5px]" icon={faEnvelope} />
+              <p>mail</p>
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faSquarePhoneFlip} />
+              <p>Hotline</p>
+            </div>
+            <div>
+              <p>thời gian làm việc</p>
+            </div>
           </div>
         </div>
       </div>
